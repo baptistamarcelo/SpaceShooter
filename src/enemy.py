600dce502809e861e4e9ade0a36c471a8e18b255
Marcelo <marceloabaptista@gmail.com>
2019-06-02 18:23:24 +1000

    Enemy movement

--- src/enemy.py
+++ src/enemy.py
@@ -8,14 +8,11 @@ class Enemy:
         self.ship = ship
         self.hit = False
         self.difficulty = difficulty
-        self.move_cooldown_y = False
-        self.move_cooldown_x = False
         self.speed_x = 0
         self.speed_y = 0
-        self.move_cooldown_y_max = 100
-        self.move_cooldown_x_max = 100
-        self.move_cooldown_y_count = 0
-        self.move_cooldown_x_count = 0
+        self.move_cooldown_x = None
+        self.move_cooldown_y = None
+        self.reset_move_cooldown()
 
     def display(self):
         self.move()
@@ -24,25 +21,37 @@ class Enemy:
 
         self.ship.display()
 
-    def move(self):
-        move_x = random.randint(1, 10)
-        move_y = random.randint(1, 20)
-        if self.move_cooldown_x:
-            if self.move_cooldown_x_count < self.move_cooldown_x_max and self.ship.pos_x > 0 and self.ship.pos_x < (W - self.ship.width):
-                self.ship.pos_x += self.ship.speed * self.speed_x
-            else:
-                self.move_cooldown_x_count = 0
-                self.move_cooldown_x = False
+    def reset_move_cooldown(self):
+        cooldown = {'active': False,
+                    'count': 0,
+                    'max': 1000}
+        self.move_cooldown_x = cooldown
+        self.move_cooldown_y = cooldown
 
-        if self.move_cooldown_y:
-            if self.move_cooldown_y_count < self.move_cooldown_y_max:
+    def move(self):
+        move_x = random.randint(1, 100)
+        move_y = random.randint(1, 300)
+        if self.move_cooldown_x['active']:
+            if self.move_cooldown_x['count'] < self.move_cooldown_x['max']:
+                self.ship.pos_x += self.ship.speed * self.speed_x
+                if self.ship.pos_x <= 0:
+                    self.ship.pos_x = 0 + self.speed_x
+                    self.reset_move_cooldown()
+                if self.ship.pos_x >= (W - self.ship.width):
+                    self.ship.pos_x = (W - self.ship.width) - self.speed_x
+                    self.reset_move_cooldown()
+            else:
+                self.reset_move_cooldown()
+
+        if self.move_cooldown_y['active']:
+            if self.move_cooldown_y['count'] < self.move_cooldown_y['max']:
                 self.ship.pos_y += self.ship.speed * self.speed_y
             else:
-                self.move_cooldown_y_count = 0
-                self.move_cooldown_y = False
+                self.move_cooldown_y['count'] = 0
+                self.move_cooldown_y['active'] = False
 
         if move_x == 1:
-            self.move_cooldown_x = True
+            self.move_cooldown_x['active'] = True
             move_left = random.randint(0, 1)
             if move_left:
                 self.speed_x = 1
@@ -50,7 +59,7 @@ class Enemy:
                 self.speed_x = -1
 
         if move_y == 1:
-            self.move_cooldown_y = True
+            self.move_cooldown_y['active'] = True
             move_down = random.randint(0, 1)
             if move_down and self.ship.pos_y < H * 0.75:
                 self.speed_y = 1
