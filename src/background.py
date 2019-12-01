42facfb855ab5c6e6c8b68b7f68452cb60094f5a
Marcelo <marceloabaptista@gmail.com>
2019-12-01 12:30:19 +1100

    Reverting background, changing bg initialization

--- src/background.py
+++ src/background.py
@@ -9,17 +9,17 @@ class Background:
         self.pos_y_1 = pos_y_1
         self.pos_y_2 = pos_y_2
         self.speed = speed
-        self.surface = pygame.transform.scale(surface, (W, H))
+        # self.surface = pygame.transform.scale(surface, (W, H))
 
     def display(self):
         screen.fill(color_black)
-        self.pos_y_1 += self.speed
-        self.pos_y_2 += self.speed
+        # self.pos_y_1 += self.speed
+        # self.pos_y_2 += self.speed
 
-        screen.blit(self.surface, (0, self.pos_y_1))
-        screen.blit(self.surface, (0, self.pos_y_2))
+        # screen.blit(self.surface, (0, self.pos_y_1))
+        # screen.blit(self.surface, (0, self.pos_y_2))
 
-        if self.pos_y_1 > H:
-            self.pos_y_1 = -H
-        if self.pos_y_2 > H:
-            self.pos_y_2 = -H
+        # if self.pos_y_1 > H:
+        #     self.pos_y_1 = -H
+        # if self.pos_y_2 > H:
+        #     self.pos_y_2 = -H
